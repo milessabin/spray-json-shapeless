3252840818395a4d5f0a11722f908575007062df
Aemon Cannon <aemoncannon@gmail.com>
2010-03-25 16:40:45 -0700

    ?

--- src/com/ensime/server/compiler.scala
+++ src/com/ensime/server/compiler.scala
@@ -11,7 +11,6 @@ import scala.tools.nsc.reporters.Reporter
 import scala.concurrent.SyncVar
 import java.io.File
 
-
 case class CompilationResultEvent(notes:List[Note])
 case class TypeCompletionResultEvent(members:List[AccessibleTypeMember], callId:SExp)
 case class ReloadFileEvent(file:File)
