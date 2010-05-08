c2c1ca0d1211f5f693c5703e5c8a453e429449e0
Aemon Cannon <aemoncannon@gmail.com>
2010-05-08 15:34:25 -0700

    Capitalize source files.

--- project/build/Ensime.scala
+++ project/build/Ensime.scala
@@ -6,6 +6,7 @@ class EnsimeProject(info: ProjectInfo) extends DefaultProject(info){
   // Copy the ensime.jar, scala-library.jar and scala-compiler.jar to 
   // the bin directory, for conveniant running.
   lazy val dist = task {
+    log.info("Copying runtime jars to ./bin....")
     copyFile(jarPath, "bin" / "ensime.jar", log)
     val deps = mainDependencies
     copyFlat(deps.scalaJars.get, path("bin"), log)
