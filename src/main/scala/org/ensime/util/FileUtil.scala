01b83d490078e3b01f72aa5cfdf2479f459fed06
Aemon Cannon <aemoncannon@gmail.com>
2010-08-20 22:56:03 -0700

    Restrict instantiation of CanonFile

--- src/main/scala/org/ensime/util/FileUtil.scala
+++ src/main/scala/org/ensime/util/FileUtil.scala
@@ -23,25 +23,28 @@ class RichFile(file: File) {
 
 }
 
-class CanonFile(path:String) extends File(path){}
-
 /** implicitely enrich java.io.File with methods of RichFile */
 object RichFile {
   implicit def toRichFile(file: File) = new RichFile(file)
 }
 
 object FileUtils {
-  
-  implicit def toRichFile(file: File) = new RichFile(file)
 
-  implicit def toCanonFile(file: File):CanonFile = {
-    try{
-      new CanonFile(file.getCanonicalPath)
-    }
-    catch{
-      case e:Exception => new CanonFile(file.getAbsolutePath)
+  class CanonFile private (path:String) extends File(path){}
+  object CanonFile {
+    def apply(file:File) = {
+      try{
+	new CanonFile(file.getCanonicalPath)
+      }
+      catch{
+	case e:Exception => new CanonFile(file.getAbsolutePath)
+      }
     }
   }
+
+  implicit def toRichFile(file: File) = new RichFile(file)
+
+  implicit def toCanonFile(file: File):CanonFile = CanonFile(file)
 
   def expandRecursively(rootDir:File, fileList:Iterable[File], isValid:(File => Boolean)):Set[CanonFile] = {
     (for(f <- fileList;
