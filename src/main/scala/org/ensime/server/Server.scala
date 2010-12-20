4e3990797fb3b2884767d8a6e4027c83763efb83
Aemon Cannon <aemoncannon@gmail.com>
2010-12-20 01:01:35 -0800

    Add proper command-line parsing to Server.scala

--- src/main/scala/org/ensime/server/Server.scala
+++ src/main/scala/org/ensime/server/Server.scala
@@ -6,46 +6,58 @@ import org.ensime.protocol._
 import org.ensime.util.WireFormat
 import scala.actors._
 import scala.actors.Actor._
+import org.github.scopt.OptionParser
 
 object Server {
   def main(args: Array[String]): Unit = {
-    try {
-      System.setProperty("actors.corePoolSize", "5")
-      System.setProperty("actors.maxPoolSize", "10")
+    System.setProperty("actors.corePoolSize", "5")
+    System.setProperty("actors.maxPoolSize", "10")
 
+    var portfile:String = ""
+    val parser = new OptionParser("PROGRAM") {
+      arg("<portfile>", "<portfile> indicates where to write the server's port", 
+	{ f:String => portfile = f })
+    }
+
+    if (parser.parse(args)) {
+
+      // TODO add an option to change the protocol
       val protocol: Protocol = SwankProtocol
 
-      // TODO use a real cmdline parser here
-      val portfile = args(0)
       val project: Project = new Project(protocol)
       project.start()
 
-      // 0 will cause socket to bind to first available port
-      val requestedPort = 0
-      val listener = new ServerSocket(requestedPort)
-      val actualPort = listener.getLocalPort
-      println("Server listening on " + actualPort + "..")
-      writePort(portfile, actualPort)
-      while (true) {
-        try {
-          val socket = listener.accept()
-          println("Got connection, creating handler...")
-          val handler = new SocketHandler(socket, protocol, project)
-          handler.start()
-        } catch {
-          case e: IOException =>
+      try {
+	// 0 will cause socket to bind to first available port
+	val requestedPort = 0
+	val listener = new ServerSocket(requestedPort)
+	val actualPort = listener.getLocalPort
+	println("Server listening on " + actualPort + "..")
+	writePort(portfile, actualPort)
+	while (true) {
+          try {
+            val socket = listener.accept()
+            println("Got connection, creating handler...")
+            val handler = new SocketHandler(socket, protocol, project)
+            handler.start()
+          } catch {
+            case e: IOException =>
             {
               System.err.println("Error in server listen loop: " + e)
             }
-        }
-      }
-      listener.close()
-    } catch {
-      case e: IOException =>
-        {
+          }
+	}
+	listener.close()
+      } catch {
+	case e: IOException =>
+	{
           System.err.println("Server listen failed: " + e)
           System.exit(-1)
-        }
+	}
+      }
+    }
+    else {
+      System.exit(0)	
     }
   }
 
@@ -57,10 +69,10 @@ object Server {
       System.out.println("Wrote port " + port + " to " + filename + ".")
     } catch {
       case e: IOException =>
-        {
-          System.err.println("Could not write port to " + filename + ". " + e)
-          System.exit(-1)
-        }
+      {
+        System.err.println("Could not write port to " + filename + ". " + e)
+        System.exit(-1)
+      }
     }
     finally {
       out.close()
@@ -83,10 +95,10 @@ class SocketHandler(socket: Socket, protocol: Protocol, project: Project) extend
         }
       } catch {
         case e: IOException =>
-          {
-            System.err.println("Error in socket reader: " + e)
-            exit('error)
-          }
+        {
+          System.err.println("Error in socket reader: " + e)
+          exit('error)
+        }
       }
     }
   }
@@ -98,10 +110,10 @@ class SocketHandler(socket: Socket, protocol: Protocol, project: Project) extend
       protocol.writeMessage(value, out)
     } catch {
       case e: IOException =>
-        {
-          System.err.println("Write to client failed: " + e)
-          exit('error)
-        }
+      {
+        System.err.println("Write to client failed: " + e)
+        exit('error)
+      }
     }
   }
 
