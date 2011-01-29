978a70fe97774a07a31b5942c9c554fc4f08f8b1
Aemon Cannon <aemoncannon@gmail.com>
2011-01-29 15:12:28 -0800

    Add bash code to emit .ensime

--- test_projects/init.sh
+++ test_projects/init.sh
@@ -1,7 +1,23 @@
+#!bash
+
+
 # Clone and update testing projects..
 
-git clone https://github.com/hdeshev/sbt-deps.git
-cd sbt-deps
+PROJ=sbt-deps
+rm -rf $PROJ
+git clone https://github.com/hdeshev/${PROJ}.git
+cd ${PROJ}
 sbt update
+read -d '' CONFIG <<EOF
+(
+:project-package "${PROJ}"
+:use-sbt t
+:sbt-subprojects (
+  (name: "web" :deps ("core"))
+  (name: "core" :deps ())
+  )
+)
+EOF
+echo $CONFIG > .ensime
 cd ..
 
