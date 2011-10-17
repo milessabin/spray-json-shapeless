a9a0a0770b4f6e598b3c5798924ffc6f4ec4e841
Aemon Cannon <aemoncannon@gmail.com>
2011-10-16 22:36:23 -0400

    Change ENSIME server to BSD licensing.

--- src/main/scala/org/ensime/server/SemanticHighlighting.scala
+++ src/main/scala/org/ensime/server/SemanticHighlighting.scala
@@ -1,27 +1,36 @@
 /**
-*  Copyright (C) 2010 Aemon Cannon
-*
-*  This program is free software; you can redistribute it and/or
-*  modify it under the terms of the GNU General Public License as
-*  published by the Free Software Foundation; either version 2 of
-*  the License, or (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public
-*  License along with this program; if not, write to the Free
-*  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-*  MA 02111-1307, USA.
+*  Copyright (c) 2010, Aemon Cannon
+*  All rights reserved.
+*  
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions are met:
+*      * Redistributions of source code must retain the above copyright
+*        notice, this list of conditions and the following disclaimer.
+*      * Redistributions in binary form must reproduce the above copyright
+*        notice, this list of conditions and the following disclaimer in the
+*        documentation and/or other materials provided with the distribution.
+*      * Neither the name of ENSIME nor the
+*        names of its contributors may be used to endorse or promote products
+*        derived from this software without specific prior written permission.
+*  
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL Aemon Cannon BE LIABLE FOR ANY
+*  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+*  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 package org.ensime.server
-import org.ensime.model.{Helpers, SymbolDesignation, SymbolDesignations}
+import org.ensime.model.{ Helpers, SymbolDesignation, SymbolDesignations }
 import scala.collection.mutable.ListBuffer
-import scala.tools.nsc.interactive.{CompilerControl, Global}
+import scala.tools.nsc.interactive.{ CompilerControl, Global }
 import scala.tools.nsc.util.RangePosition
+import scala.math
 
 trait SemanticHighlighting { self: Global with Helpers =>
 
@@ -82,7 +91,7 @@ trait SemanticHighlighting { self: Global with Helpers =>
               add('varField)
             } else if (sym.isValue) {
               add('valField)
-            }  
+            }
           }
           case Select(qual, selector: Name) => {
             val sym = t.symbol
@@ -105,7 +114,7 @@ trait SemanticHighlighting { self: Global with Helpers =>
                 addAt(start, end, 'varField)
               } else if (under.isValue) {
                 addAt(start, end, 'valField)
-              } 
+              }
             } else if (sym.isConstructor) {
               val start = try { sym.pos.start }
               catch { case _ => treeP.start }
@@ -134,17 +143,17 @@ trait SemanticHighlighting { self: Global with Helpers =>
           case ValDef(mods, name, tpt, rhs) => {
             val sym = t.symbol
 
-	    // TODO:
-	    // Unfotunately t.symbol.pos returns a RangePosition
-	    // that covers the entire declaration. 
-	    //
-	    // This is brittle, but I don't know a better 
-	    // way to get the position of just the name.
+            // TODO:
+            // Unfotunately t.symbol.pos returns a RangePosition
+            // that covers the entire declaration. 
+            //
+            // This is brittle, but I don't know a better 
+            // way to get the position of just the name.
 
-	    val start = if (mods.positions.isEmpty) t.pos.start
-	    else mods.positions.map(_._2.end).max + 2
-	    val len = name.decode.length()
-	    val end = start + len
+            val start = if (mods.positions.isEmpty) t.pos.start
+            else mods.positions.map(_._2.end).max + 2
+            val len = name.decode.length()
+            val end = start + len
             val isField = sym.owner.isType || sym.owner.isModule
 
             if (mods.isParameter) {
@@ -170,17 +179,16 @@ trait SemanticHighlighting { self: Global with Helpers =>
               addAt(start, end, 'class)
             } else if (sym.isModule) {
               addAt(start, end, 'object)
-            }
-	    else if(t.tpe != null){
-	      // TODO:
-	      // This case occurs when 
-	      // pattern matching on 
-	      // case classes. 
-	      // As in:
-	      // case MyClass(a:Int,b:Int)
-	      //
-	      // Works, but this is *way* under-constrained.
-	      addAt(start, end, 'object)
+            } else if (t.tpe != null) {
+              // TODO:
+              // This case occurs when 
+              // pattern matching on 
+              // case classes. 
+              // As in:
+              // case MyClass(a:Int,b:Int)
+              //
+              // Works, but this is *way* under-constrained.
+              addAt(start, end, 'object)
             }
           }
           case _ => {}
@@ -190,14 +198,24 @@ trait SemanticHighlighting { self: Global with Helpers =>
     }
   }
 
-  protected def symbolDesignationsInRegion(p: RangePosition, 
+  protected def symbolDesignationsInRegion(p: RangePosition,
     tpes: List[scala.Symbol]): SymbolDesignations = {
     val tpeSet = Set[scala.Symbol]() ++ tpes
-    val traverser = new SymDesigsTraverser(p, tpeSet)
-    val typed:Response[Tree] = new Response[Tree]
+    val typed: Response[Tree] = new Response[Tree]
     askType(p.source, false, typed)
     typed.get.left.toOption match {
       case Some(tree) => {
+
+//        val cu = unitOf(p.source)
+//        var startOfProblems = p.end
+//        for (prob <- cu.problems) {
+//	  if(prob.severityLevel >= 2){
+//            startOfProblems = math.min(
+//              prob.pos.start, startOfProblems)
+//	  }
+//        }
+
+	val traverser = new SymDesigsTraverser(p, tpeSet)
         traverser.traverse(tree)
         SymbolDesignations(
           p.source.file.path,
