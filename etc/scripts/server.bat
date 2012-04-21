ff8b77775a171ab936715b11e40f832795318fd0
Aemon Cannon <aemoncannon@gmail.com>
2012-04-21 18:54:45 -0400

    Get rid of escape analysis flag in server scripts.

--- etc/scripts/server.bat
+++ etc/scripts/server.bat
@@ -1,4 +1,4 @@
 set PORT_FILE=%1
 set CLASSPATH=<RUNTIME_CLASSPATH>
-if "%ENSIME_JVM_ARGS%"=="" (set ENSIME_JVM_ARGS=-XX:+DoEscapeAnalysis -Xms256M -Xmx1512M -XX:PermSize=128m -Xss1M -Dfile.encoding=UTF-8)
+if "%ENSIME_JVM_ARGS%"=="" (set ENSIME_JVM_ARGS=-Xms256M -Xmx1512M -XX:PermSize=128m -Xss1M -Dfile.encoding=UTF-8)
 java -classpath %CLASSPATH% %ENSIME_JVM_ARGS% org.ensime.server.Server %PORT_FILE%
