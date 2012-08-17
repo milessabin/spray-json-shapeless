b0fa466f3c9ca2b9741e65a72c3bf6241d8abcd6
Eugene Burmako <xeno.by@gmail.com>
2012-08-17 23:10:41 +0200

    allows double quotes in ENSIME_JVM_ARGS

--- etc/scripts/server.bat
+++ etc/scripts/server.bat
@@ -1,5 +1,5 @@
 set PORT_FILE=%1
 set BOOTCLASSPATH=<RUNTIME_BOOTCLASSPATH>
 set CLASSPATH=<RUNTIME_CLASSPATH>
-if "%ENSIME_JVM_ARGS%"=="" (set ENSIME_JVM_ARGS=-Xms256M -Xmx1512M -XX:PermSize=128m -Xss1M -Dfile.encoding=UTF-8)
+if not defined ENSIME_JVM_ARGS (set ENSIME_JVM_ARGS=-Xms256M -Xmx1512M -XX:PermSize=128m -Xss1M -Dfile.encoding=UTF-8)
 java -Xbootclasspath/a:%BOOTCLASSPATH% -classpath %CLASSPATH% %ENSIME_JVM_ARGS% org.ensime.server.Server %PORT_FILE%
