aa5b7cbf341a09bd63f17563fc0e305ad683a8f9
Aemon Cannon <aemoncannon@gmail.com>
2013-02-03 17:01:32 -0500

    Up version

--- project/Build.scala
+++ project/Build.scala
@@ -66,7 +66,7 @@ object EnsimeBuild extends Build {
       base = file ("."),
       settings = Project.defaultSettings ++
       Seq(
-        version := "0.9.8.3",
+        version := "0.9.8.4",
         organization := "org.ensime",
         scalaVersion := TwoTenVersion,
         crossScalaVersions := Seq(TwoNineVersion, TwoTenVersion),
