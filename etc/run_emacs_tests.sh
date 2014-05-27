fa11c981f68fea206aff0de2eff6618bcb07c672
Eric Daniel <eric-github@barberic.org>
2014-05-26 23:24:02 -0700

    run_emacs_tests.sh takes a test name as argument

--- etc/run_emacs_tests.sh
+++ etc/run_emacs_tests.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-emacs --no-init-file --load .emacs_test.el
+
+if [ $# -ge 1 ]; then
+  emacs --no-init-file --load .emacs_test.el --eval '(ensime-run-one-test "'"$*"'")'
+else
+  emacs --no-init-file --load .emacs_test.el  --eval '(ensime-run-all-tests)'
+fi
