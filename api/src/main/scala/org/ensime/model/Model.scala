rorygraves/ensime-server

Change from long strings to ThreadId

api/src/main/scala/org/ensime/model/Model.scala
@@ -2,9 +2,6 @@ package org.ensime.model
 
 import java.io.File
 import org.ensime.util.FileEdit
-import scala.collection.mutable
-
-import org.ensime.config._
 
 sealed trait EntityInfo {
   def name: String
@@ -162,7 +159,23 @@ sealed trait DebugLocation
 
 case class DebugObjectReference(objectId: Long) extends DebugLocation
 
-case class DebugStackSlot(threadId: String, frame: Int, offset: Int) extends DebugLocation
+/**
+ * A debugger thread id.
+ */
+case class ThreadId(id: Long)
+
+object ThreadId {
+  /**
+   * Create a ThreadId from a String representation
+   * @param s A Long encoded as a string
+   * @return A ThreadId
+   */
+  def apply(s: String): ThreadId = {
+    new ThreadId(s.toLong)
+  }
+}
+
+case class DebugStackSlot(threadId: ThreadId, frame: Int, offset: Int) extends DebugLocation
 
 case class DebugArrayElement(objectId: Long, index: Int) extends DebugLocation
 

server/src/main/scala/org/ensime/server/protocol/swank/SwankProtocol.scala
@@ -561,7 +561,6 @@ class SwankProtocol(actorSystem: ActorSystem,
        */
       case ("swank:init-project", Nil) =>
         sendRPCAckOK(callId)
-        rpcTarget.rpcNotifyClientReady()
         rpcTarget.rpcSubscribeAsync((e) => { sendEvent(e) })
 
       /**
@@ -588,8 +587,8 @@ class SwankProtocol(actorSystem: ActorSystem,
        */
       case ("swank:peek-undo", Nil) =>
         rpcTarget.rpcPeekUndo() match {
-          case Right(result) => sendRPCReturn(toWF(result), callId)
-          case Left(msg) => sendRPCError(ErrPeekUndoFailed, msg, callId)
+          case Some(result) => sendRPCReturn(toWF(result), callId)
+          case None => sendRPCError(ErrPeekUndoFailed, "No such undo.", callId)
         }
       /**
        * Doc RPC:
@@ -1528,7 +1527,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-continue", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugContinue(threadId)
+        val result = rpcTarget.rpcDebugContinue(ThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1547,7 +1546,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-step", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugStep(threadId)
+        val result = rpcTarget.rpcDebugStep(ThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1566,7 +1565,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-next", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugNext(threadId)
+        val result = rpcTarget.rpcDebugNext(ThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1585,7 +1584,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-step-out", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugStepOut(threadId)
+        val result = rpcTarget.rpcDebugStepOut(ThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1605,7 +1604,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok (:slot :thread-id "7" :frame 2 :offset 0)) 42)
        */
       case ("swank:debug-locate-name", StringAtom(threadId) :: StringAtom(name) :: Nil) =>
-        val result = rpcTarget.rpcDebugLocateName(threadId, name)
+        val result = rpcTarget.rpcDebugLocateName(ThreadId(threadId), name)
         result match {
           case Some(loc) =>
             sendRPCReturn(toWF(loc), callId)
@@ -1654,7 +1653,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok "A little lamb") 42)
        */
       case ("swank:debug-to-string", StringAtom(threadId) :: DebugLocationExtractor(loc) :: Nil) =>
-        val result = rpcTarget.rpcDebugToString(threadId, loc)
+        val result = rpcTarget.rpcDebugToString(ThreadId(threadId), loc)
         result match {
           case Some(value) => sendRPCReturn(toWF(value), callId)
           case None => sendRPCReturn(toWF(value = false), callId)
@@ -1697,7 +1696,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok (:frames () :thread-id "23" :thread-name "main")) 42)
        */
       case ("swank:debug-backtrace", StringAtom(threadId) :: IntAtom(index) :: IntAtom(count) :: Nil) =>
-        val result = rpcTarget.rpcDebugBacktrace(threadId, index, count)
+        val result = rpcTarget.rpcDebugBacktrace(ThreadId(threadId), index, count)
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1853,11 +1852,11 @@ object SwankProtocol {
           }
         case SymbolAtom("slot") =>
           for (
-            StringAtom(id) <- m.get(key(":thread-id"));
+            StringAtom(threadId) <- m.get(key(":thread-id"));
             IntAtom(frame) <- m.get(key(":frame"));
             IntAtom(offset) <- m.get(key(":offset"))
           ) yield {
-            DebugStackSlot(id, frame, offset)
+            DebugStackSlot(ThreadId(threadId), frame, offset)
           }
         case _ => None
       }

server/src/test/scala/org/ensime/server/protocol/swank/SwankProtocolSpec.scala
@@ -88,15 +88,15 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
       val file3_str = fileToWireString(file3)
 
       testWithResponse("""(swank:peek-undo)""") { (t, m, id) =>
-        (t.rpcPeekUndo _).expects().returns(Right(Undo(3, "Undoing stuff", List(TextEdit(file3, 5, 7, "aaa")))))
+        (t.rpcPeekUndo _).expects().returns(Some(Undo(3, "Undoing stuff", List(TextEdit(file3, 5, 7, "aaa")))))
         (m.send _).expects(s"""(:return (:ok (:id 3 :summary "Undoing stuff" :changes ((:type edit :file $file3_str :from 5 :to 7 :text "aaa")))) $id)""")
       }
     }
 
     it("should understand swank:peek-undo - fail") {
       testWithResponse("""(swank:peek-undo)""") { (t, m, id) =>
-        (t.rpcPeekUndo _).expects().returns(Left("ErrorError"))
-        (m.send _).expects(s"""(:return (:abort 206 "ErrorError") $id)""")
+        (t.rpcPeekUndo _).expects().returns(None)
+        (m.send _).expects(s"""(:return (:abort 206 "No such undo.") $id)""")
       }
     }
 
@@ -588,35 +588,35 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
 
     it("should understand swank:debug-continue") {
       testWithResponse("""(swank:debug-continue "1")""") { (t, m, id) =>
-        (t.rpcDebugContinue _).expects("1").returns(true)
+        (t.rpcDebugContinue _).expects(ThreadId(1)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-step") {
       testWithResponse("""(swank:debug-step "982398123")""") { (t, m, id) =>
-        (t.rpcDebugStep _).expects("982398123").returns(true)
+        (t.rpcDebugStep _).expects(ThreadId(982398123)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-next") {
       testWithResponse("""(swank:debug-next "982398123")""") { (t, m, id) =>
-        (t.rpcDebugNext _).expects("982398123").returns(true)
+        (t.rpcDebugNext _).expects(ThreadId(982398123)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-step-out") {
       testWithResponse("""(swank:debug-step-out "982398123")""") { (t, m, id) =>
-        (t.rpcDebugStepOut _).expects("982398123").returns(true)
+        (t.rpcDebugStepOut _).expects(ThreadId(982398123)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-locate-name") {
       testWithResponse("""(swank:debug-locate-name "7" "apple")""") { (t, m, id) =>
-        (t.rpcDebugLocateName _).expects("7", "apple").returns(Some(debugLocObjectRef))
+        (t.rpcDebugLocateName _).expects(ThreadId(7), "apple").returns(Some(debugLocObjectRef))
         (m.send _).expects("(:return (:ok " + debugLocObjectRefStr + ") " + id + ")")
       }
     }
@@ -644,14 +644,14 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
 
     it("should understand swank:debug-value - stack slot") {
       testWithResponse("""(swank:debug-value (:type slot :thread-id "23" :frame 7 :offset 25))""") { (t, m, id) =>
-        (t.rpcDebugValue _).expects(DebugStackSlot("23", 7, 25)).returns(Some(debugStringValue))
+        (t.rpcDebugValue _).expects(DebugStackSlot(ThreadId(23), 7, 25)).returns(Some(debugStringValue))
         (m.send _).expects("(:return (:ok " + debugStringValueStr + ") " + id + ")")
       }
     }
 
     it("should understand swank:debug-to-string - array element") {
       testWithResponse("""(swank:debug-to-string "2" (:type element :object-id "23" :index 2))""") { (t, m, id) =>
-        (t.rpcDebugToString _).expects("2", DebugArrayElement(23L, 2)).returns(Some("null"))
+        (t.rpcDebugToString _).expects(ThreadId(2), DebugArrayElement(23L, 2)).returns(Some("null"))
         (m.send _).expects("(:return (:ok \"null\") " + id + ")")
       }
     }
@@ -665,7 +665,7 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
 
     it("should understand swank:debug-backtrace") {
       testWithResponse("""(swank:debug-backtrace "23" 0 2)""") { (t, m, id) =>
-        (t.rpcDebugBacktrace _).expects("23", 0, 2).returns(debugBacktrace)
+        (t.rpcDebugBacktrace _).expects(ThreadId(23), 0, 2).returns(debugBacktrace)
         (m.send _).expects("(:return (:ok " + debugBacktraceStr + ") " + id + ")")
       }
     }

milessabin/spray-json-shapeless

api/src/main/scala/org/ensime/model/Model.scala
@@ -2,9 +2,6 @@ package org.ensime.model
 
 import java.io.File
 import org.ensime.util.FileEdit
-import scala.collection.mutable
-
-import org.ensime.config._
 
 sealed trait EntityInfo {
   def name: String
@@ -162,7 +159,23 @@ sealed trait DebugLocation
 
 case class DebugObjectReference(objectId: Long) extends DebugLocation
 
-case class DebugStackSlot(threadId: String, frame: Int, offset: Int) extends DebugLocation
+/**
+ * A debugger thread id.
+ */
+case class DebugThreadId(id: Long)
+
+object DebugThreadId {
+  /**
+   * Create a ThreadId from a String representation
+   * @param s A Long encoded as a string
+   * @return A ThreadId
+   */
+  def apply(s: String): DebugThreadId = {
+    new DebugThreadId(s.toLong)
+  }
+}
+
+case class DebugStackSlot(threadId: DebugThreadId, frame: Int, offset: Int) extends DebugLocation
 
 case class DebugArrayElement(objectId: Long, index: Int) extends DebugLocation
 

server/src/main/scala/org/ensime/server/protocol/swank/SwankProtocol.scala
@@ -564,7 +564,6 @@ class SwankProtocol(actorSystem: ActorSystem,
        */
       case ("swank:init-project", Nil) =>
         sendRPCAckOK(callId)
-        rpcTarget.rpcNotifyClientReady()
         rpcTarget.rpcSubscribeAsync((e) => { sendEvent(e) })
 
       /**
@@ -591,8 +590,8 @@ class SwankProtocol(actorSystem: ActorSystem,
        */
       case ("swank:peek-undo", Nil) =>
         rpcTarget.rpcPeekUndo() match {
-          case Right(result) => sendRPCReturn(toWF(result), callId)
-          case Left(msg) => sendRPCError(ErrPeekUndoFailed, msg, callId)
+          case Some(result) => sendRPCReturn(toWF(result), callId)
+          case None => sendRPCError(ErrPeekUndoFailed, "No such undo.", callId)
         }
       /**
        * Doc RPC:
@@ -1534,7 +1533,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-continue", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugContinue(threadId)
+        val result = rpcTarget.rpcDebugContinue(DebugThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1553,7 +1552,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-step", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugStep(threadId)
+        val result = rpcTarget.rpcDebugStep(DebugThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1572,7 +1571,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-next", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugNext(threadId)
+        val result = rpcTarget.rpcDebugNext(DebugThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1591,7 +1590,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok t) 42)
        */
       case ("swank:debug-step-out", StringAtom(threadId) :: Nil) =>
-        val result = rpcTarget.rpcDebugStepOut(threadId)
+        val result = rpcTarget.rpcDebugStepOut(DebugThreadId(threadId))
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1611,7 +1610,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok (:slot :thread-id "7" :frame 2 :offset 0)) 42)
        */
       case ("swank:debug-locate-name", StringAtom(threadId) :: StringAtom(name) :: Nil) =>
-        val result = rpcTarget.rpcDebugLocateName(threadId, name)
+        val result = rpcTarget.rpcDebugLocateName(DebugThreadId(threadId), name)
         result match {
           case Some(loc) =>
             sendRPCReturn(toWF(loc), callId)
@@ -1660,7 +1659,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok "A little lamb") 42)
        */
       case ("swank:debug-to-string", StringAtom(threadId) :: DebugLocationExtractor(loc) :: Nil) =>
-        val result = rpcTarget.rpcDebugToString(threadId, loc)
+        val result = rpcTarget.rpcDebugToString(DebugThreadId(threadId), loc)
         result match {
           case Some(value) => sendRPCReturn(toWF(value), callId)
           case None => sendRPCReturn(toWF(value = false), callId)
@@ -1703,7 +1702,7 @@ class SwankProtocol(actorSystem: ActorSystem,
        *   (:return (:ok (:frames () :thread-id "23" :thread-name "main")) 42)
        */
       case ("swank:debug-backtrace", StringAtom(threadId) :: IntAtom(index) :: IntAtom(count) :: Nil) =>
-        val result = rpcTarget.rpcDebugBacktrace(threadId, index, count)
+        val result = rpcTarget.rpcDebugBacktrace(DebugThreadId(threadId), index, count)
         sendRPCReturn(toWF(result), callId)
 
       /**
@@ -1859,11 +1858,11 @@ object SwankProtocol {
           }
         case SymbolAtom("slot") =>
           for (
-            StringAtom(id) <- m.get(key(":thread-id"));
+            StringAtom(threadId) <- m.get(key(":thread-id"));
             IntAtom(frame) <- m.get(key(":frame"));
             IntAtom(offset) <- m.get(key(":offset"))
           ) yield {
-            DebugStackSlot(id, frame, offset)
+            DebugStackSlot(DebugThreadId(threadId), frame, offset)
           }
         case _ => None
       }

server/src/test/scala/org/ensime/server/protocol/swank/SwankProtocolSpec.scala
@@ -88,15 +88,15 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
       val file3_str = fileToWireString(file3)
 
       testWithResponse("""(swank:peek-undo)""") { (t, m, id) =>
-        (t.rpcPeekUndo _).expects().returns(Right(Undo(3, "Undoing stuff", List(TextEdit(file3, 5, 7, "aaa")))))
+        (t.rpcPeekUndo _).expects().returns(Some(Undo(3, "Undoing stuff", List(TextEdit(file3, 5, 7, "aaa")))))
         (m.send _).expects(s"""(:return (:ok (:id 3 :summary "Undoing stuff" :changes ((:type edit :file $file3_str :from 5 :to 7 :text "aaa")))) $id)""")
       }
     }
 
     it("should understand swank:peek-undo - fail") {
       testWithResponse("""(swank:peek-undo)""") { (t, m, id) =>
-        (t.rpcPeekUndo _).expects().returns(Left("ErrorError"))
-        (m.send _).expects(s"""(:return (:abort 206 "ErrorError") $id)""")
+        (t.rpcPeekUndo _).expects().returns(None)
+        (m.send _).expects(s"""(:return (:abort 206 "No such undo.") $id)""")
       }
     }
 
@@ -588,35 +588,35 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
 
     it("should understand swank:debug-continue") {
       testWithResponse("""(swank:debug-continue "1")""") { (t, m, id) =>
-        (t.rpcDebugContinue _).expects("1").returns(true)
+        (t.rpcDebugContinue _).expects(DebugThreadId(1)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-step") {
       testWithResponse("""(swank:debug-step "982398123")""") { (t, m, id) =>
-        (t.rpcDebugStep _).expects("982398123").returns(true)
+        (t.rpcDebugStep _).expects(DebugThreadId(982398123)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-next") {
       testWithResponse("""(swank:debug-next "982398123")""") { (t, m, id) =>
-        (t.rpcDebugNext _).expects("982398123").returns(true)
+        (t.rpcDebugNext _).expects(DebugThreadId(982398123)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-step-out") {
       testWithResponse("""(swank:debug-step-out "982398123")""") { (t, m, id) =>
-        (t.rpcDebugStepOut _).expects("982398123").returns(true)
+        (t.rpcDebugStepOut _).expects(DebugThreadId(982398123)).returns(true)
         (m.send _).expects(s"""(:return (:ok t) $id)""")
       }
     }
 
     it("should understand swank:debug-locate-name") {
       testWithResponse("""(swank:debug-locate-name "7" "apple")""") { (t, m, id) =>
-        (t.rpcDebugLocateName _).expects("7", "apple").returns(Some(debugLocObjectRef))
+        (t.rpcDebugLocateName _).expects(DebugThreadId(7), "apple").returns(Some(debugLocObjectRef))
         (m.send _).expects("(:return (:ok " + debugLocObjectRefStr + ") " + id + ")")
       }
     }
@@ -644,14 +644,14 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
 
     it("should understand swank:debug-value - stack slot") {
       testWithResponse("""(swank:debug-value (:type slot :thread-id "23" :frame 7 :offset 25))""") { (t, m, id) =>
-        (t.rpcDebugValue _).expects(DebugStackSlot("23", 7, 25)).returns(Some(debugStringValue))
+        (t.rpcDebugValue _).expects(DebugStackSlot(DebugThreadId(23), 7, 25)).returns(Some(debugStringValue))
         (m.send _).expects("(:return (:ok " + debugStringValueStr + ") " + id + ")")
       }
     }
 
     it("should understand swank:debug-to-string - array element") {
       testWithResponse("""(swank:debug-to-string "2" (:type element :object-id "23" :index 2))""") { (t, m, id) =>
-        (t.rpcDebugToString _).expects("2", DebugArrayElement(23L, 2)).returns(Some("null"))
+        (t.rpcDebugToString _).expects(DebugThreadId(2), DebugArrayElement(23L, 2)).returns(Some("null"))
         (m.send _).expects("(:return (:ok \"null\") " + id + ")")
       }
     }
@@ -665,7 +665,7 @@ class SwankProtocolSpec extends FunSpec with ShouldMatchers with BeforeAndAfterA
 
     it("should understand swank:debug-backtrace") {
       testWithResponse("""(swank:debug-backtrace "23" 0 2)""") { (t, m, id) =>
-        (t.rpcDebugBacktrace _).expects("23", 0, 2).returns(debugBacktrace)
+        (t.rpcDebugBacktrace _).expects(DebugThreadId(23), 0, 2).returns(debugBacktrace)
         (m.send _).expects("(:return (:ok " + debugBacktraceStr + ") " + id + ")")
       }
     }