d55a5daed2fcafcc42279bc8241a228f715c2b48
Sam Halliday <sam.halliday@gmail.com>
2015-06-19 18:16:56 +0100

    correction to file watching workaround close #61

--- core/src/main/scala/org/ensime/indexer/FileWatchers.scala
+++ core/src/main/scala/org/ensime/indexer/FileWatchers.scala
@@ -61,26 +61,46 @@ class ClassfileWatcher(
 
   // WORKAROUND https://issues.apache.org/jira/browse/VFS-536
   // We don't have a dedicated test for this because it is an upstream bug
-  private val workaround = new DefaultFileMonitor(new FileListener {
-    private def targets =
-      config.compileClasspath.filter(_.isDirectory).map(vfs.vfile).map(_.getName)
-    def watched(event: FileChangeEvent) = {
-      val dir = event.getFile
-      val name = dir.getName
-      targets.exists(name.isAncestor)
+  private val workaround = new DefaultFileMonitor(
+    new FileListener {
+      private def targets: Set[FileName] = for {
+        dir <- config.targetClasspath
+        ref = vfs.vfile(dir)
+      } yield ref.getName()
+
+      def watched(event: FileChangeEvent) = {
+        val dir = event.getFile
+        val changed = dir.getName
+        // read backwards as: "changed is an ancestor of target"
+        targets.exists { target => target isAncestor changed }
+      }
+      def fileChanged(event: FileChangeEvent): Unit = {
+        if (watched(event)) {
+          // a fast delete followed by a create looks like a change
+          log.debug(s"${event.getFile} was possibly recreated")
+          reset()
+        }
+      }
+      def fileCreated(event: FileChangeEvent): Unit = {
+        if (watched(event)) {
+          log.debug(s"${event.getFile} was created")
+          reset()
+        }
+      }
+      def fileDeleted(event: FileChangeEvent): Unit = {
+        if (watched(event)) {
+          log.debug(s"${event.getFile} was deleted")
+          // nothing to do, we need to wait for it to return
+        }
+      }
     }
-    def fileChanged(event: FileChangeEvent): Unit =
-      if (watched(event)) reset()
-    def fileCreated(event: FileChangeEvent): Unit =
-      if (watched(event)) reset()
-    def fileDeleted(event: FileChangeEvent): Unit = {}
-  })
+  )
 
   workaround.setRecursive(false)
   workaround.start()
 
-  private def ancestors(f: File): List[File] = {
-    val parent = f.getParentFile
+  private def ancestors(f: FileObject): List[FileObject] = {
+    val parent = f.getParent
     if (parent == null) Nil
     else parent :: ancestors(parent)
   }
@@ -89,20 +109,28 @@ class ClassfileWatcher(
   // calling reset() a lot of times. We should probably debounce it, but
   // it seems to happen so quickly that no damage is done.
   private def reset(): Unit = {
+    // When this triggers we tend to see it multiple times because
+    // we're watching the various depths within the project.
     log.info("Setting up new file watchers")
-    // must remove then add to avoid leaks
 
+    val root = vfs.vfile(config.root)
+
+    // must remove then add to avoid leaks
     for {
       d <- config.targetClasspath
-      _ = fm.removeFile(vfs.vfile(d))
-      _ = fm.addFile(vfs.vfile(d))
-      ancestor <- ancestors(d)
-      if config.root.contains(ancestor)
-      _ = workaround.removeFile(vfs.vfile(ancestor))
-      _ = workaround.addFile(vfs.vfile(ancestor))
-    } workaround.removeFile(vfs.vfile(config.root))
+      dir = vfs.vfile(d)
+      _ = fm.removeFile(dir)
+      _ = fm.addFile(dir)
+      ancestor <- ancestors(dir)
+      if ancestor.getName isAncestor root.getName
+      _ = workaround.removeFile(ancestor)
+      _ = workaround.addFile(ancestor)
+    } {
+      // side effects in the for comprehension
+    }
 
-    workaround.addFile(vfs.vfile(config.root))
+    workaround.removeFile(root)
+    workaround.addFile(root)
   }
 
   reset()
